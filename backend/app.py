2a6e138e4244736aa8c159e9b97e9ca7dcb56560
Jika <jiksboy2000@gmail.com>
2025-03-21 18:11:53 +0100

    login page styles

--- backend/app.py
+++ backend/app.py
@@ -13,13 +13,19 @@ app.config['JWT_TOKEN_LOCATION'] = ['headers']
 jwt = JWTManager(app)
 
 # Enable CORS
-CORS(app, origins=["http://localhost:5173"])
+# CORS(app, origins="http://localhost:5173")
+CORS(app,origins="*")
+
+
 
 # Simulated admin users (in-memory storage)
 admins = {
     "admin1": {"password": "password123"},
     "admin2": {"password": "securepass"}
 }
+@app.route('/')
+def home():
+    return "WELCOME TO MSSN ABUAD"
 
 @app.route('/api/login', methods=['POST'])
 def login():
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
 import Home from "./pages/Home";
 import Programs from "./pages/Programs";
 import Store from "./pages/Store";
@@ -7,16 +8,12 @@ import QandA from "./pages/QandA";
 import Blog from "./pages/Blog";
 import Events from "./pages/Events";
 import Donate from "./pages/Donate";
-import Admin from "./pages/Admin";
 import News from "./pages/News";
-import Footer from "./components/Footer";
-import Login from "./pages/Login";
-import PrivateRoute from "./components/PrivateRoute";
-
 import MssnHistory from "./pages/MssnHistory";
 import MssnAbuadHistory from "./pages/MssnAbuadHistory";
+
+import Admin from "./pages/Admin";
 import Login from "./pages/Login";
-import AppLayout from "./components/AppLayout";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
@@ -34,7 +31,14 @@ function App() {
           <Route path="/events" element={<Events />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="/news" element={<News />} />
-          <Route path="/admin" element={<PrivateRoute <Admin /></PrivateRoute>}/>
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <Admin />
+              </PrivateRoute>
+            }
+          />
         </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
